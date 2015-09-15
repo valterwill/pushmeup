e33b81e076227bfd28321783e827496b124c92f8
Leandro Souza <leandroalvesdesouza@gmail.com>
2015-09-15 17:17:05 -0300

    adding a default value to silent notification param, while instantiating the notification sender

--- lib/pushmeup/apns/notification.rb
+++ lib/pushmeup/apns/notification.rb
@@ -1,8 +1,8 @@
 module APNS
   class Notification
     attr_accessor :device_token, :alert, :badge, :sound, :other, :silent
-    
-    def initialize(device_token, message, silent)
+
+    def initialize(device_token, message, silent=false)
       self.device_token = device_token
       if message.is_a?(Hash)
         self.alert = message[:alert]
@@ -16,17 +16,17 @@ module APNS
         raise "Notification needs to have either a hash or string"
       end
     end
-        
+
     def packaged_notification
       pt = self.packaged_token
       pm = self.packaged_message
       [0, 0, 32, pt, 0, pm.bytesize, pm].pack("ccca*cca*")
     end
-  
+
     def packaged_token
       [device_token.gsub(/[\s|<|>]/,'')].pack('H*')
     end
-  
+
     def packaged_message
       aps = {'aps'=> {} }
       aps['aps']['alert'] = self.alert if self.alert
@@ -36,6 +36,6 @@ module APNS
       aps.merge!(self.other) if self.other
       aps.to_json.gsub(/\\u([\da-fA-F]{4})/) {|m| [$1].pack("H*").unpack("n*").pack("U*")}
     end
-    
+
   end
 end
